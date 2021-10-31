f634a9876cb499322d48990c3d55494312684797
donsabater <wassapdon2000@yahoo.com>
2021-10-30 18:42:58 -0700

    added set_results_post reducer

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,9 +1,9 @@
-import { SET_RECENT_POSTS } from "../actions/types";
+import { SET_RECENT_POSTS, SET_RESULTS_POSTS}  from "../actions/types";
 
 
 
 const INIT_STATE = {
-    posts: [],
+    resultsPosts: [],
     recentPosts: []
 }
 
@@ -16,6 +16,14 @@ export default function(state = INIT_STATE, action) {
               ...state,
                  recentPosts
             }
+
+            case SET_RESULTS_POSTS:
+                const resultsPosts = action.payload;
+                return {
+                    ...state, 
+                    posts: resultsPosts
+                }
+
         default:
             return state;
     }
