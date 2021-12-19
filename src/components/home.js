e637e784b5d971c0d6df66c010a4f43b47d6dfa3
donsabater <wassapdon2000@yahoo.com>
2021-12-18 16:13:35 -0800

    styled results component searchbar

--- src/components/home.js
+++ src/components/home.js
@@ -20,7 +20,7 @@ class Home extends Component {
       <div className="home">
       
        <Logo/>
-       <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
+       <SearchBar page="home" onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
        <RecentPosts/>
       </div>
     );
--- src/components/results.js
+++ src/components/results.js
@@ -19,7 +19,7 @@ class Results extends Component {
         return (
             <div className="results">
                 <Logo size={55}/>
-                <SearchBar onSubmit={(query) => this.handleSearchbarSubmit(query)}/>
+                <SearchBar page="results" onSubmit={(query) => this.handleSearchbarSubmit(query)}/>
                 <ResultsPosts/>
 
             </div>
